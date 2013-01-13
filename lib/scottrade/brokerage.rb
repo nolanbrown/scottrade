ef1058636e2e14c0b435c3d82e4086d5dd01ae04
Nolan B <nolanbrown@gmail.com>
2013-01-12 21:08:01 -0800

    Simplified Brokerage class.

--- lib/scottrade/brokerage.rb
+++ lib/scottrade/brokerage.rb
@@ -14,50 +14,47 @@ module Scottrade
     attr_reader :accounts, :positions
     attr_reader :current_market_value, :todays_percent_change, :todays_value_change
     
+    KEYS_TO_VARIABLES = {
+      # accounts data keys
+      "TotalAccountValue"                         => { :key => :total_value, :is_money => true },
+      "TotalMoneyBalance"                         => { :key => :total_cash_balance, :is_money => true },
+      "YesterdayTotalMoneyBalance"                => { :key => :yesterday_total_cash_balance, :is_money => true },
+      "AvailableFreeCash"                         => { :key => :available_free_cash_blanace, :is_money => true },
+      "TotalMarketValueWithOptions"               => { :key => :total_market_value_with_options, :is_money => true },
+      "TotalMarketValueNoOptions"                 => { :key => :total_market_value_without_options, :is_money => true },
+      "YesterdayTotalMarketValue"                 => { :key => :yesterday_total_market_value, :is_money => true },
+      "TotalSettledFunds"                         => { :key => :total_settled_funds, :is_money => true },
+      "TotalUnsettledSells"                       => { :key => :total_unsettled_sells, :is_money => true },
+      "FundsAvailableToBuyNonMarginables"         => { :key => :funds_available_to_buy_non_marginables, :is_money => true },
+      "FundsAvailableToBuyMarginables"            => { :key => :funds_available_to_buy_marginables, :is_money => true },
+      "FundsAvailableToBuyOptions"                => { :key => :funds_available_to_buy_options, :is_money => true },
+      "FundsAvailableToBuyMutualFunds"            => { :key => :funds_available_to_buy_mutual_funds, :is_money => true },
+      "FundsAvailableForWithdraw"                 => { :key => :funds_available_for_withdraw, :is_money => true },
+      "TodaysChangeApproxLiquidationValNoOptions" => { :key => :approximate_liquidation_value, :is_money => true },
+      "BrokerageAccountBalance"                   => { :key => :account_balance, :is_money => true },
+      # positions data keys
+      "totalMktValue"                             => { :key => :current_market_value, :is_money => false },
+      "totalPctChange"                            => { :key => :todays_percent_change,  :is_money => false },
+      "toalPriceChange"                           => { :key => :todays_value_change, :is_money => false },
+    }
+
+    
     def initialize(session)
       @session = session
     end
     
-    # def positions
-    # 
-    # end
-    # def accounts
-    # end
-    
     def update_accounts
       params = request_parameters("GetFrontEndMoneyBalances")
             
-      response = @session.post(params)
-      parsed_response = JSON.parse(response.body)
-      if parsed_response["error"] == "false"
-        @total_value = Money.parse parsed_response["TotalAccountValue"]
-        @total_cash_balance = Money.parse parsed_response["TotalMoneyBalance"]
-        @yesterday_total_cash_balance = Money.parse parsed_response["YesterdayTotalMoneyBalance"]
-        @available_free_cash_blanace = Money.parse parsed_response["AvailableFreeCash"]
-        @total_market_value_with_options = Money.parse parsed_response["TotalMarketValueWithOptions"]
-        @total_market_value_without_options = Money.parse parsed_response["TotalMarketValueNoOptions"]
-        @yesterday_total_market_value = Money.parse parsed_response["YesterdayTotalMarketValue"]
-        @total_settled_funds = Money.parse parsed_response["TotalSettledFunds"]
-        @total_unsettled_sells = Money.parse parsed_response["TotalUnsettledSells"]
-        @funds_available_to_buy_non_marginables = Money.parse parsed_response["FundsAvailableToBuyNonMarginables"]
-        @funds_available_to_buy_marginables = Money.parse parsed_response["FundsAvailableToBuyMarginables"]
-        @funds_available_to_buy_options = Money.parse parsed_response["FundsAvailableToBuyOptions"]
-        @funds_available_to_buy_mutual_funds = Money.parse parsed_response["FundsAvailableToBuyMutualFunds"]
-        @funds_available_for_withdraw = Money.parse parsed_response["FundsAvailableForWithdraw"]
-        @approximate_liquidation_value = Money.parse parsed_response["TodaysChangeApproxLiquidationValNoOptions"]
-        @account_balance = Money.parse parsed_response["BrokerageAccountBalance"]
-        
-        @accounts = []
-        unparsed_accounts = parsed_response["AccTypeBalances"]
-        unparsed_accounts.each{|acct|
-          @accounts.push Account.new(acct)
-        }
-        
-      elsif parsed_response["msg"]
-        raise RequestError, parsed_response["msg"]
-      else
-        raise RequestError
-      end
+      response = session_post(params)
+      set_variables_from_response(response)
+
+      @accounts = []
+      unparsed_accounts = response["AccTypeBalances"]
+      unparsed_accounts.each{|acct|
+        @accounts.push Account.new(acct)
+      }
+      
     end
     
     def update_positions
@@ -67,27 +64,39 @@ module Scottrade
       params["returnRealTimeMktValue"] = "true"
       
       params["serviceID"] = "GetPositions_v2"
-            
+      
+      response = session_post(params)
+      
+      set_variables_from_response(response)
+      
+      @positions = []
+      all_positions = response["Positions"]
+      all_positions.each{|pos|
+        @positions.push Position.new(pos)
+      }
+    end    
+    
+    private
+    def session_post(params)
       response = @session.post(params)
       parsed_response = JSON.parse(response.body)
       if parsed_response["error"] == "false"
-
-        @current_market_value = parsed_response["totalMktValue"]
-        @todays_percent_change = parsed_response["totalPctChange"]
-        @todays_value_change = parsed_response["toalPriceChange"]
-        
-        @positions = []
-        all_positions = parsed_response["Positions"]
-        all_positions.each{|pos|
-          @positions.push Position.new(pos)
-        }
-        
+        return parsed_response
       elsif parsed_response["msg"]
         raise RequestError, parsed_response["msg"]
       else
         raise RequestError
       end
-    end    
+    end
     
+    def set_variables_from_response(response)
+      response.each{|key,value|
+        settings = KEYS_TO_VARIABLES[key]
+        if settings[:is_money]
+          value = Money.parse value
+        end
+        instance_variable_set("@#{settings[:key].to_s}", value)
+      }
+    end
   end
 end
